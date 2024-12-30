c4a56952ddb10e83088c17abf08895027fb34a7b
rddi-8 <reddi.osum@gmail.com>
2024-12-30 15:42:12 +0100

    slightly more condensed item display

--- scenes/items_container.gd
+++ scenes/items_container.gd
@@ -4,7 +4,7 @@ extends Control
 
 func add_item(item :Item):
 	var new_control := Control.new()
-	new_control.custom_minimum_size = Vector2(64, 64)
+	new_control.custom_minimum_size = Vector2(56, 56)
 	new_control.mouse_filter = Control.MOUSE_FILTER_IGNORE
 	layout_container.add_child(new_control)
 	new_control.add_child(item)
