7ec736ed6f782a77e417e0a1ebff80e20f7f80b9
jwapobie <stardreambelike@gmail.com>
2025-01-04 20:53:40 -0500

    hide integration with engine feature

--- objects/neuro_integration_option_button.gd
+++ objects/neuro_integration_option_button.gd
@@ -4,6 +4,8 @@ extends OptionButton
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	item_selected.connect(select_option)
+	if !OS.has_feature('neuro_integration'):
+		get_parent().visible = false
 
 func select_option(option: int) -> void:
 	GameState.neuro_integration_mode = option
