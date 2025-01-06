4578163638d12d1b3427ef73a1c3070f04a4990c
jwapobie <stardreambelike@gmail.com>
2025-01-06 11:52:48 -0500

    fix tooltip positioning on bottom edge

--- objects/tooltip/tooltip.gd
+++ objects/tooltip/tooltip.gd
@@ -3,12 +3,18 @@ extends Control
 
 @onready var item_description: LabelParser = %ItemDescription
 
+var desired_position: Vector2 = Vector2.ZERO
+
+func _process(delta: float) -> void:
+	if visible:
+		var realpos: Vector2 = Vector2(0,0)
+		realpos.x = min(desired_position.x, get_viewport_rect().size.x - $PanelContainer.size.x)
+		realpos.y = min(desired_position.y, get_viewport_rect().size.y - $PanelContainer.size.y)
+		self.position = realpos
 
 func show_tooltip(bb_text :String, position :Vector2):
 	item_description.parse_text(bb_text)
-	position.x = min(position.x, get_viewport_rect().size.x - $PanelContainer.size.x)
-	position.y = min(position.y, get_viewport_rect().size.y - $PanelContainer.size.y)
-	self.position = position
+	desired_position = position
 	visible = true
 
 func hide_tooltip():
