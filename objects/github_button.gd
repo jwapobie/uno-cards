b2c3a8c286f5b6695993a0cc4cc7744b70a1183b
rddi-8 <reddi.osum@gmail.com>
2025-01-07 00:26:42 +0100

    url tooltip

--- objects/github_button.gd
+++ objects/github_button.gd
@@ -3,3 +3,14 @@ extends Button
 
 func _pressed() -> void:
 	OS.shell_open("https://github.com/jwapobie/uno-cards")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	mouse_entered.connect(on_hover)
+	mouse_exited.connect(on_unhover)
+
+func on_hover():
+	Tooltip.show_tooltip("https://github.com/jwapobie/uno-cards", get_global_mouse_position())
+
+func on_unhover():
+	Tooltip.hide_tooltip()
